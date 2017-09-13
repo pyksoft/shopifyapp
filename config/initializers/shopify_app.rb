2d8532c9a373fe23273213072c4cba47ca699de1
Harry Pan <harrypbz@outlook.com>
2017-09-13 19:11:20 +0200

    redirect url

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,8 @@
 ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
-  config.api_key = "<api_key>"
-  config.secret = "<secret>"
+  config.api_key = "c1defe33b8ff58218ca9162ae1dd74e4"
+  config.secret = "4f9ad383351af4142183902f43a092ad"
+  config.redirect_url="http://validate-address.herokuapp.com"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
   config.after_authenticate_job = false
