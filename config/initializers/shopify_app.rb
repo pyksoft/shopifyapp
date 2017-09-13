711d91104d10b0678baa0bfeaf99846c415a7109
Harry Pan <harrypbz@outlook.com>
2017-09-13 20:19:47 +0200

    change api key

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,8 +1,7 @@
 ShopifyApp.configure do |config|
-  config.application_name = "My Shopify App"
-  config.api_key = "c1defe33b8ff58218ca9162ae1dd74e4"
-  config.secret = "4f9ad383351af4142183902f43a092ad"
-  #config.redirect_url="http://validate-address.herokuapp.com"
+  config.application_name = "Validate Address"
+  config.api_key = "3b44b20048330a892a2425a6c0d17bd0"
+  config.secret = "a546adcdb98b91fd3605c03e1e7cf873"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
   config.after_authenticate_job = false
