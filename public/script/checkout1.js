3e5f533e6e5a572233d6bedeeab598e901d01cd1
Harry Pan <harrypbz@outlook.com>
2017-09-20 22:22:21 +0200

    changed js

--- public/script/checkout1.js
+++ public/script/checkout1.js
@@ -50,7 +50,7 @@ var formatUnitStreet = ["Australia", "Canada", "France", "Hong Kong", "Malaysia"
                     i = t.address_components.find(function(e) {
                         return -1 !== e.types.indexOf("administrative_area_level_2")
                     }),
-                   c = t.address_components.find(function(e) {
+                    c = t.address_components.find(function(e) {
                         return -1 !== e.types.indexOf("administrative_area_level_1")
                     }),
                     l = t.address_components.find(function(e) {
@@ -80,7 +80,7 @@ var formatUnitStreet = ["Australia", "Canada", "France", "Hong Kong", "Malaysia"
             e.setAttribute("id", "addressValidatorBox"), document.getElementsByClassName("main__header")[0].appendChild(e)
         },
         o = function(n, s) {
-            var o = new XMLHttpRequest;            
+            var o = new XMLHttpRequest;
             o.open("POST", e + "/checkout_update"), o.setRequestHeader("Content-Type", "application/json;charset=UTF-8"), o.send(JSON.stringify({
                 shop: t,
                 token: Shopify.Checkout.token,
@@ -112,10 +112,15 @@ var formatUnitStreet = ["Australia", "Canada", "France", "Hong Kong", "Malaysia"
                 s = d("numAlertWrapper", e.textStreetNum || "&#9888; Please specify a street number.");
             n && !t ? cityParent.insertBefore(s, cityParent.childNodes[0]) : !n && t && document.getElementById("numAlertWrapper").remove()
         };
-    if (Shopify.Checkout && "contact_information" === Shopify.Checkout.step) {        
+    if (Shopify.Checkout && "contact_information" === Shopify.Checkout.step) {
         var i = new XMLHttpRequest;
-        //i.open("GET", e + "/brand_preferences?shop=" + t, !0), i.send(null), i.onreadystatechange = function() {
-            //if (4 === i.readyState) {
+        /*
+        e = "https://app.roboturk.co/address_validator/api",
+        t = Shopify.shop || Shopify.Checkout.apiHost,*/        
+        i.open("GET", e + "/brand_preferences?shop=" + t, !0), 
+        i.send(null), 
+        i.onreadystatechange = function() {
+            if (4 === i.readyState) {
                 var e = JSON.parse(i.responseText);
                 if (e.disable_po_boxes ? (address_2 && (address_2.onkeyup = function() {
                         return r(e.customization)
@@ -129,13 +134,13 @@ var formatUnitStreet = ["Australia", "Canada", "France", "Hong Kong", "Malaysia"
                     var t = document.createElement("script");
                     t.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyABMvMXqWmgepYbmi8fnm2zm9pW2ECgPq0&libraries=places&callback=initializeAutocomplete", t.type = "text/javascript", head.appendChild(t)
                 }
-            //}
-        //}
+            }
+        }
     }
     var c = void 0,
         l = ["done", "invalid order", "no shipping address", "already fulfilled", "invalid request", "invalid store", "invalid country", "invalid checkout", "ignore order"];
     if (Shopify.Checkout && "shipping_method" === Shopify.Checkout.step) {
-        var u = new XMLHttpRequest;        
+        var u = new XMLHttpRequest;
         u.open("GET", e + "/brand_preferences?shop=" + t, !0), u.send(null), u.onreadystatechange = function() {
             if (4 === u.readyState && JSON.parse(u.responseText).validate_pre_checkout && "write_script_tags,read_orders,write_orders,read_checkouts,write_checkouts" === JSON.parse(u.responseText).scope) {
                 c = JSON.parse(u.responseText).customization;
