a45adb9d9530ffded43fa2abb703aafeaf075052
Harry Pan <harrypbz@outlook.com>
2017-09-26 11:34:38 +0200

    index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index  
     @option = Option.find(1)    
-    @shop = params[:shop]
+    @shop = ShopifyAPI::Shop.current
   end
 end
