e0f0178856b99383c6fa7c3149b5217b1ca399c1
Harry Pan <harrypbz@outlook.com>
2017-09-27 09:47:33 +0200

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root :to => 'home#index'
   mount ShopifyApp::Engine, at: '/'
-  resources :options, only: [:update]
+  resources :options, only: [:update, :edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #get '/options', to: 'options#show'
 end
