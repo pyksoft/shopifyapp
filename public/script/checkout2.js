b226100925d0c1debb90936378d417de78a018ef
Harry Pan <harrypbz@outlook.com>
2017-09-27 19:52:19 +0200

    changed js

--- public/script/checkout2.js
+++ public/script/checkout2.js
@@ -9,8 +9,6 @@ var formatUnitStreet = ["Australia", "Canada", "France", "Hong Kong", "Malaysia"
     zip = document.getElementById("checkout_shipping_address_zip") || document.getElementById("checkout_billing_address_zip") || document.getElementById("checkout_shipping_address_attributes_zip"),
     state = document.getElementById("checkout_shipping_address_province") || document.getElementById("checkout_billing_address_province") || document.getElementById("checkout_shipping_address_attributes_province"),
     country = document.getElementById("checkout_shipping_address_country") || document.getElementById("checkout_billing_address_country") || document.getElementById("checkout_shipping_address_attributes_country"),
-    option,
-    
     initializeAutocomplete = function() {
         window.Checkout.$("[data-google-places]").removeAttr("data-google-places");
         var e = new google.maps.places.Autocomplete(address_1, {
@@ -26,17 +24,7 @@ var formatUnitStreet = ["Australia", "Canada", "France", "Hong Kong", "Malaysia"
                 }
             })
         };
-        e.addListener("place_changed", function() {            
-            
-            if (option && option.pobox_warning) {
-                
-                address_1 && r1("");
-            }
-
-            if (option && option.streetnum_warning) {                
-                address_1 && a1("");
-            }
-
+        e.addListener("place_changed", function() {
             var t = e.getPlace();
             if (t.address_components) {
                 var n = t.address_components.find(function(e) {
@@ -115,62 +103,62 @@ var formatUnitStreet = ["Australia", "Canada", "France", "Hong Kong", "Malaysia"
         });
     }
 
-    var n1 = function() {
-        document.getElementById("addressValidatorBox").style.backgroundColor = c.colorBackground, document.getElementById("addressValidatorBox").style.borderColor = c.colorBorder, document.getElementById("addressValidatorBox").style.color = c.colorText, document.getElementById("addressValidatorBox").getElementsByTagName("h2")[0] && (document.getElementById("addressValidatorBox").getElementsByTagName("h2")[0].style.color = c.colorText), [].forEach.call(document.getElementsByClassName("changed"), function(e) {
-            return e.style.color = c.colorHighlight
-        });
-        var e = "#suggestedAddress:hover, #originalAddress:hover { background-color: " + c.colorHover + "!important };",
-            t = document.createElement("style");
-        t.styleSheet ? t.styleSheet.cssText = e : t.appendChild(document.createTextNode(e)), document.getElementsByTagName("head")[0].appendChild(t)
-    },
-    s1 = function() {
-        var e = document.createElement("div");
-        e.setAttribute("id", "addressValidatorBox"), document.getElementsByClassName("main__header")[0].appendChild(e)
-    },
-    o1 = function(n, s) {
-        var o = new XMLHttpRequest;
-        o.open("POST", e + "/checkout_update"), o.setRequestHeader("Content-Type", "application/json;charset=UTF-8"), o.send(JSON.stringify({
-            shop: t,
-            token: Shopify.Checkout.token,
-            status: n
-        })), "updated" === n ? (document.getElementById("addressValidatorBox").innerHTML = "<h2>" + c.textSelectSuggestion + "</h2>", document.getElementsByClassName("review-block__content")[0] ? document.getElementsByClassName("review-block__content")[0].innerHTML = s : document.querySelector(".content-box__row.content-box__row--secondary p") ? document.querySelector(".content-box__row.content-box__row--secondary p").innerHTML = s : document.getElementById("shipping-address-recap") && (document.getElementById("shipping-address-recap").innerHTML = s)) : document.getElementById("addressValidatorBox").innerHTML = "<h2>" + c.textSelectConfirm + "</h2>", document.getElementById("addressValidatorBox").getElementsByTagName("h2")[0].style.color = c.colorText
-    },
-    d1 = function(e, t) {
-        var n = document.createElement("div");
-        return n.setAttribute("id", e), n.style.borderRadius = "5px", n.style.marginBottom = "10px", n.style.padding = "10px", n.style.border = "1px solid #e59400", n.style.color = "#e59400", n.style.backgroundColor = "#fff6e5", n.innerHTML = t, n
-    },
-    r1 = function(e) {
-        var n = document.getElementById("addressAlertWrapper"),
-            s = function(e) {
-                var t = e.replace(/[^\w\s]/gi, "").replace(/ /g, "").toLowerCase(),
-                    n = -1 !== t.indexOf("box") && t.length < 8,
-                    s = -1 !== t.indexOf("pobox") && t.length < 12;
-                return n || s
-            },
-            o = function(e) {
-                return -1 !== ["letussellit.myshopify.com", "buymobilerapidco.myshopify.com", "buy-mobile-new-zealand.myshopify.com", "from-china-with-love-australia.myshopify.com", "vayahss.myshopify.com", "amaysim.myshopify.com"].indexOf(t) && -1 !== e.replace(/[^\w\s]/gi, "").replace(/ /g, "").toLowerCase().indexOf("parcellocker")
-            },
-            r = s(address_1.value) || s(address_2.value) || o(address_1.value) || o(address_2.value),
-            a = d("addressAlertWrapper", "&#9888; We cannot deliver to a P.O. Box. Please provide a valid street address.");
-        r && !n ? cityParent.insertBefore(a, cityParent.childNodes[0]) : !r && n && document.getElementById("addressAlertWrapper").remove()
-    },
-    a1 = function(e) {
-        var t = document.getElementById("numAlertWrapper"),
-            n = !/^\d/.test(address_1.value) && address_1.value.length >= 6,
-            s = d("numAlertWrapper", "&#9888; Please specify a street number.");
-        
-        if(n && !t) {
-            cityParent.insertBefore(s, cityParent.childNodes[0]);
-        }            
-        if(!n && t) {
-            document.getElementById("numAlertWrapper").remove()
-        }
-        //n && !t ? cityParent.insertBefore(s, cityParent.childNodes[0]) : !n && t && document.getElementById("numAlertWrapper").remove()
-    };
-
 ! function() {
     var e = "https://app.roboturk.co/address_validator/api",
-        t = Shopify.shop || Shopify.Checkout.apiHost;        
+        t = Shopify.shop || Shopify.Checkout.apiHost,
+        n = function() {
+            document.getElementById("addressValidatorBox").style.backgroundColor = c.colorBackground, document.getElementById("addressValidatorBox").style.borderColor = c.colorBorder, document.getElementById("addressValidatorBox").style.color = c.colorText, document.getElementById("addressValidatorBox").getElementsByTagName("h2")[0] && (document.getElementById("addressValidatorBox").getElementsByTagName("h2")[0].style.color = c.colorText), [].forEach.call(document.getElementsByClassName("changed"), function(e) {
+                return e.style.color = c.colorHighlight
+            });
+            var e = "#suggestedAddress:hover, #originalAddress:hover { background-color: " + c.colorHover + "!important };",
+                t = document.createElement("style");
+            t.styleSheet ? t.styleSheet.cssText = e : t.appendChild(document.createTextNode(e)), document.getElementsByTagName("head")[0].appendChild(t)
+        },
+        s = function() {
+            var e = document.createElement("div");
+            e.setAttribute("id", "addressValidatorBox"), document.getElementsByClassName("main__header")[0].appendChild(e)
+        },
+        o = function(n, s) {
+            var o = new XMLHttpRequest;
+            o.open("POST", e + "/checkout_update"), o.setRequestHeader("Content-Type", "application/json;charset=UTF-8"), o.send(JSON.stringify({
+                shop: t,
+                token: Shopify.Checkout.token,
+                status: n
+            })), "updated" === n ? (document.getElementById("addressValidatorBox").innerHTML = "<h2>" + c.textSelectSuggestion + "</h2>", document.getElementsByClassName("review-block__content")[0] ? document.getElementsByClassName("review-block__content")[0].innerHTML = s : document.querySelector(".content-box__row.content-box__row--secondary p") ? document.querySelector(".content-box__row.content-box__row--secondary p").innerHTML = s : document.getElementById("shipping-address-recap") && (document.getElementById("shipping-address-recap").innerHTML = s)) : document.getElementById("addressValidatorBox").innerHTML = "<h2>" + c.textSelectConfirm + "</h2>", document.getElementById("addressValidatorBox").getElementsByTagName("h2")[0].style.color = c.colorText
+        },
+        d = function(e, t) {
+            var n = document.createElement("div");
+            return n.setAttribute("id", e), n.style.borderRadius = "5px", n.style.marginBottom = "10px", n.style.padding = "10px", n.style.border = "1px solid #e59400", n.style.color = "#e59400", n.style.backgroundColor = "#fff6e5", n.innerHTML = t, n
+        },
+        r = function(e) {
+            var n = document.getElementById("addressAlertWrapper"),
+                s = function(e) {
+                    var t = e.replace(/[^\w\s]/gi, "").replace(/ /g, "").toLowerCase(),
+                        n = -1 !== t.indexOf("box") && t.length < 8,
+                        s = -1 !== t.indexOf("pobox") && t.length < 12;
+                    return n || s
+                },
+                o = function(e) {
+                    return -1 !== ["letussellit.myshopify.com", "buymobilerapidco.myshopify.com", "buy-mobile-new-zealand.myshopify.com", "from-china-with-love-australia.myshopify.com", "vayahss.myshopify.com", "amaysim.myshopify.com"].indexOf(t) && -1 !== e.replace(/[^\w\s]/gi, "").replace(/ /g, "").toLowerCase().indexOf("parcellocker")
+                },
+                r = s(address_1.value) || s(address_2.value) || o(address_1.value) || o(address_2.value),
+                a = d("addressAlertWrapper", "&#9888; We cannot deliver to a P.O. Box. Please provide a valid street address.");
+            r && !n ? cityParent.insertBefore(a, cityParent.childNodes[0]) : !r && n && document.getElementById("addressAlertWrapper").remove()
+        },
+        a = function(e) {
+            var t = document.getElementById("numAlertWrapper"),
+                n = !/^\d/.test(address_1.value) && address_1.value.length >= 6,
+                s = d("numAlertWrapper", "&#9888; Please specify a street number.");
+            
+            if(n && !t) {
+                cityParent.insertBefore(s, cityParent.childNodes[0]);
+            }
+            
+            if(!n && t) {
+                document.getElementById("numAlertWrapper").remove()
+            }
+            //n && !t ? cityParent.insertBefore(s, cityParent.childNodes[0]) : !n && t && document.getElementById("numAlertWrapper").remove()
+        };
 
     if (Shopify.Checkout && "contact_information" === Shopify.Checkout.step) {        
         var i = new XMLHttpRequest;
@@ -183,8 +171,6 @@ var formatUnitStreet = ["Australia", "Canada", "France", "Hong Kong", "Malaysia"
           if (4 === i.readyState) {
             var e = JSON.parse(i.responseText),
                 o = e.option;
-
-            option = o;
 
             /*
             if (o.pobox_warning ? (address_2 && (address_2.onkeyup = function() {
@@ -203,17 +189,17 @@ var formatUnitStreet = ["Australia", "Canada", "France", "Hong Kong", "Malaysia"
             if (o.pobox_warning) {
                 
                 address_1 && (address_1.onkeyup = function() {
-                    return r1("");
+                    return r("");
                 });
 
                 address_2 && (address_2.onkeyup = function() {
-                    return r1("");
+                    return r("");
                 });
             }
 
             if (o.streetnum_warning) {                
                 address_1 && (address_1.onkeyup = function() {
-                    return a1("");
+                    return a("");
                 });
             }
             
