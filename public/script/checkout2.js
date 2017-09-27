df76d29b99792f943fe1b3cc500f74c730a5aca8
Harry Pan <harrypbz@outlook.com>
2017-09-27 10:16:16 +0200

    changed api url in cehckout js

--- public/script/checkout2.js
+++ public/script/checkout2.js
@@ -168,7 +168,7 @@ var formatUnitStreet = ["Australia", "Canada", "France", "Hong Kong", "Malaysia"
 
 
         var i = new XMLHttpRequest;
-        e = "https://validate-address.herokuapp.com/options",
+        e = "https://validate-address.herokuapp.com/",
         t = Shopify.shop || Shopify.Checkout.apiHost,
         i.open("GET", e + "/options?shop=" + t, !0),
         //i.open("GET", e, !0),
