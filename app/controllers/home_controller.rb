b1a0f90b506795109af9051f078999b99f11a032
Harry Pan <harrypbz@outlook.com>
2017-09-28 22:26:10 +0200

    add customization

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,6 +7,13 @@ class HomeController < ShopifyApp::AuthenticatedController
       @option.domain = @shop.domain
       @option.save
     end
+
+    @customization = @option.customization
+    if @customization.nil?
+      @customization = Customization.new
+      @customization.option = @option
+      @customization.save
+    end
   end
 
   private
