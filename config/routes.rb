9693591176118bbccf9d50decbed6174836a196d
Harry Pan <harrypbz@outlook.com>
2017-09-29 18:29:14 +0200

    changed nav

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   mount ShopifyApp::Engine, at: '/'
   resources :options, only: [:update, :edit]
   resources :customizations, only: [:update, :edit]
+  #resources :options
+  #resources :customizations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/options', to: 'options#show'  
 end
